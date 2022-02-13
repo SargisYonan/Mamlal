89641d9543a2566ca720faf997c1e498cedb04a9
Sargis Yonan <sargis@yonan.org>
2022-02-13 01:34:38 -0800

    changed about section

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -7,15 +7,15 @@ type Props = {
 
 export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="ܒܘܕ ܛܐܠܬܐ" isOpen={isOpen} handleClose={handleClose}>
-      <p className="text-3xl text-gray-500 dark:text-gray-300">
-        ܡܡܠܠ - ܛܐܠܬܐ ܕܡܠ̈ܐ{' '}
+    <BaseModal title="ܒܘܕ ܕܐܗܐ ܛܐܠܬܐ" isOpen={isOpen} handleClose={handleClose}>
+      <p className="text-2xl text-gray-500 dark:text-gray-300">
+         ܡܡܠܠ - ܛܐܠܬܐ ܕܡܠ̈ܐ{' '}
+         ܀
         <a
           href="https://yonan.org/"
           className="underline font-bold"
         >
-        ܀
-          ܒܪܝܢܐ ܕܐܗܐ ܛܐܠܬܐ
+          ܒܪܝܢܐ ܕܡܡܠܠ
         </a>{' '}
       </p>
     </BaseModal>
