b80fe77ec7c657ae54844586a12831175ddf03c8
Emil Soleyman-Zomalan <emil@soleyman.com>
2022-02-21 10:13:45 -0600

    Fix whitespace issues

--- src/components/modals/SettingsDropdown.tsx
+++ src/components/modals/SettingsDropdown.tsx
@@ -7,7 +7,7 @@ type Props = {
 }
 
 export const SettingsDropdown = ({ settingName, flag, handleFlag }: Props) => {
-  
+
   const Dropdown = classnames(
     'form-select form-select-lg mb-3 appearance-none block w-full px-4 py-2 text-2xl font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
   )
@@ -24,10 +24,10 @@ export const SettingsDropdown = ({ settingName, flag, handleFlag }: Props) => {
       </h2>
       <div>
         <select className={Dropdown} onChange={selectChange} defaultValue={flag} dir="rtl">
-            <option value="eastern">ܡܕܢܚܝܐ</option>            
+            <option value="eastern">ܡܕܢܚܝܐ</option>
             <option value="estrangela">ܐܣܛܪܢܓܠܐ</option>
-            <option value="western">ܣܪܛܐ</option>            
-        </select>            
+            <option value="western">ܣܪܛܐ</option>
+        </select>
         </div>
     </div>
   );
